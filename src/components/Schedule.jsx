073e15b880c80fa0c6f01f8ef534a3e913f4cd0f
Jahnavi Majumder <majumderj811@gmail.com>
2023-12-24 16:19:31 +0000

    Blackhole left

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -4,97 +4,67 @@ import blackhole from "../imageForWebsite/blackhole.png";
 
 function Schedule() {
   return (
-    <div className="flex">
-      <div className="Mainapp flex flex-col text-white bg-none ml-16">
-        <div className="Schtitle font-Michroma text-4xl md:text-7xl flex justify-start pt-40 pr-80 md:justify-center md:pl-20">
-          <span>SCHE</span>
-          <span className="text-[#27A5EF]">DULE</span>
-        </div>
-        <div className="mainBody flex justify-center items-center pt-40 md:pt-72 pr-64 pb-44 md:pb-72 ml-32 lg:ml-0">
-          <div className="daycon flex flex-col pb-20 pr-20 md:w-4/5">
-            <div className="day1 flex flex-col mb-32">
-              <div className="daytitle font-Changa text-4xl md:text-7xl text-slate-200 underline underline-offset-4 font-thin shrink-0 hover:text-7xl md:hover:text-8xl duration-300 delay-100 pr-32 pb-16">
-                DAY1
-              </div>
-              <div className="daycon flex flex-col">
-                <div className="content text-lg flex md:hover:text-2xl duration-200 delay-100 md:text-xl">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
+    <>
+      <div className="flex flex-col md:flex-row">
+        <div className="Mainapp flex flex-col text-white ml-4 md:ml-16">
+          <div className="Schtitle font-Michroma text-3xl md:text-5xl flex justify-center pt-8 md:pt-40 pr-4 md:pr-80">
+            <span>SCHE</span>
+            <span className="text-[#27A5EF]">DULE</span>
+          </div>
+          <div className="mainBody flex justify-center items-center pt-8 md:pt-40 pr-4 md:pr-64 pb-8 md:pb-44">
+            <div className="daycon flex flex-col md:w-4/5">
+              <div className="day1 flex flex-col mb-8 md:mb-32">
+                <div className="daytitle font-Changa text-4xl md:text-7xl text-slate-200 underline underline-offset-4 font-thin hover:text-7xl md:hover:text-8xl duration-300 delay-100 mb-4">
+                  DAY1
                 </div>
-                <div className="content text-lg flex md:hover:text-2xl duration-200 delay-100 md:text-xl">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
-                </div>
-                <div className="content text-lg flex md:hover:text-2xl duration-200 delay-100 md:text-xl">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
+                <div className="daycon flex flex-col">
+                  {Array.from({ length: 3 }, (_, index) => (
+                    <div
+                      key={index}
+                      className="content flex text-sm md:text-base md:hover:text-lg duration-200 delay-100 mb-2"
+                    >
+                      <div className="p-2 shrink-0">
+                        <img
+                          src={bulletin}
+                          alt="bulletin"
+                          className="h-5 animate-spin-slow hover:animate-spin delay-150"
+                        />
+                      </div>
+                      <div className="p-2">This place is for content</div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
-            <div className="day2 flex flex-col mt-20 md:mt-32">
-              <div className="daytitle font-Changa text-4xl md:text-7xl text-slate-200 underline underline-offset-4 font-thin shrink-0 hover:text-7xl md:hover:text-8xl duration-200 delay-100 pr-32 pb-16">
-                DAY2
-              </div>
-              <div className="daycon flex flex-col">
-                <div className="content flex text-lg md:hover:text-2xl duration-200 delay-100 md:text-xl">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin-slow delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
+              <div className="day2 flex flex-col mt-4 md:mt-0">
+                <div className="daytitle font-Changa text-4xl md:text-7xl text-slate-200 underline underline-offset-4 font-thin hover:text-7xl md:hover:text-8xl duration-200 delay-100 mb-4">
+                  DAY2
                 </div>
-                <div className="content flex text-lg md:hover:text-2xl duration-200 delay-100 md:text-xl">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
-                </div>
-                <div className="content flex text-lg md:text-xl md:hover:text-2xl duration-200 delay-100">
-                  <div className="p-8 shrink-0">
-                    <img
-                      src={bulletin}
-                      alt="bulletin"
-                      className="h-9 animate-spin-slow hover:animate-spin delay-150"
-                    />
-                  </div>
-                  <div className="p-8">This place is for content</div>
+                <div className="daycon flex flex-col">
+                  {Array.from({ length: 3 }, (_, index) => (
+                    <div
+                      key={index}
+                      className="content flex text-sm md:text-base md:hover:text-lg duration-200 delay-100 mb-2"
+                    >
+                      <div className="p-2 shrink-0">
+                        <img
+                          src={bulletin}
+                          alt="bulletin"
+                          className="h-5 animate-spin-slow hover:animate-spin-slow delay-150"
+                        />
+                      </div>
+                      <div className="p-2">This place is for content</div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* Blackhole Image */}
-      <div className="blackhole-container flex justify-end items-end relative right-0">
+      <div className="blackhole-container hidden md:flex justify-end items-end relative right-0">
         <img src={blackhole} alt="blackhole" className="h-full w-140%" />
       </div>
-    </div>
+    </>
   );
 }
 
