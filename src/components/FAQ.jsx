eb11fd84c14ddd15e80f8f91ebd3ea4f799fade9
unknown <alirashid.38@yahoo.com>
2023-12-29 03:36:19 +0530

    -added a starry bg with twinkling stars, -divided FAQ section -about us responsiveness fixed - three community sponsors - centered judges

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,14 +1,15 @@
-import "../index.css";
-import FAQList from "./FAQList";
-import Wireframe from "../imageForWebsite/Wireframe.webp";
+import FAQList from "./FAQList"; // Assuming this imports the FAQ list component
 
 export default function FAQ() {
   return (
-    <div className="font-Michroma bg-cover bg-center flex flex-col md:grid md:grid-cols-3 max-md:flex-col mb-10">
-      <div className="flex-grow flex flex-col justify-center items-center text-center py-10 md:col-span-2">
-        <h1 className='text-white mb-8 text-8xl md:pl-96 md:ml-36 md:pb-24 max-md:text-4xl'>FAQs</h1>
-        <FAQList />
-        <div className="flex items-start justify-start text-center pt-8">
+    <div className="font-Michroma bg-cover bg-center flex flex-col max-md:flex-col mb-10">
+      <h1 className='text-white mb-8 text-8xl md:mt-12 md:mb-12 max-md:text-4xl'>FAQs</h1>
+      <div className="flex-grow flex flex-col justify-center items-center text-center py-10 ">
+        <div className="max-md:grid max-md:grid-cols-1  md:gap-20">
+          {/* Displaying FAQ items in two columns for desktop view */}
+          <FAQList />
+        </div>
+        <div className="flex items-start justify-start text-center pt-12">
           <a
             href="https://drive.google.com/file/d/1mBNDdQdZjdECvB5jtT9Od40kfTQVJJp9/view?usp=sharing"
             className="bg-transparent hover:bg-[#27A5EF] text-white font-semibold hover:text-black py-2 px-4 border border-[#27A5EF] hover:border-transparent rounded"
@@ -16,8 +17,6 @@ export default function FAQ() {
             CODE OF CONDUCT
           </a>
         </div>
-      </div>
-      <div className="hidden md:flex md:flex-col md:items-end md:justify-end h-screen">
       </div>
     </div>
   );
