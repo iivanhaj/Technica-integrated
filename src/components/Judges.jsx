503abe130144a127392f086e3c36663319d6c3a5
Jahnavi Majumder <majumderj811@gmail.com>
2023-12-30 00:22:59 +0530

    responsive for small and large screens only

--- src/components/Judges.jsx
+++ src/components/Judges.jsx
@@ -13,163 +13,46 @@ function Judges() {
       </h1>
       <div className="min-h-screen bg-black flex flex-col justify-center py-6 sm:py-12">
         <div className="p-6 container mx-auto">
-          <div className="md:grid md:gap-6 md:grid-cols-2 lg:grid-cols-3 justify-center">
-            <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
-              <div className="relative mb-4 rounded-2xl">
-                <img
-                  className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
-                  src={judge1}
-                  alt="Sakshi Saini"
-                />
-                <LinkedInLink
-                  profileUrl={
-                    "https://www.linkedin.com/in/sakshi-saini-741414181/"
-                  }
-                />
-              </div>
-              <div className="flex justify-between items-center w-full pb-4 mb-auto">
-                <div className="flex items-center">
-                  <div className="flex flex-1">
-                    <div>
-                      <p className="text-sm font-semibold">
-                        Sakshi <span className="text-[#27A5EF]">Saini</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-end">
-                  <div className="text-sm flex items-center text-white gap-2">
-                    HR Associate
-                    <BsPersonLinesFill className="" />
-                  </div>
-                </div>
-              </div>
-              <h3 className="font-medium text-xl leading-8">
-                <a
-                  href="#2"
-                  className="block relative group-hover:text-[#27A5EF] text-left transition-colors duration-200"
-                >
-                  Sakshi has been awarded an MBA degree in Human Resources from
-                  UPES, Dehradun. Currently serving as a HR Associate at Value
-                  Creed and doing end to end recruitment globally.
-                </a>
-              </h3>
-            </article>
-            <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
-              <div className="relative mb-4 rounded-2xl">
-                <img
-                  className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
-                  src={judge2}
-                  alt="Samridhi"
-                />
-                <LinkedInLink
-                  profileUrl={"https://www.linkedin.com/in/samridhi-soni/"}
-                />
-              </div>
-              <div className="flex justify-between items-center w-full pb-4 mb-auto">
-                <div className="flex items-center">
-                  <div className="flex flex-1">
-                    <div>
-                      <p className="text-sm font-semibold">
-                        Samridhi <span className="text-[#27A5EF]">Soni</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-end">
-                  <div className="text-sm flex items-center text-white gap-2">
-                    Senior Manager Corporate HR
-                    <BsPersonVideo2 />
-                  </div>
-                </div>
-              </div>
-              <h3 className="font-medium text-xl leading-8">
-                <a
-                  href="#2"
-                  className="block relative group-hover:text-[#27A5EF] text-left transition-colors duration-200"
-                >
-                  Samridhi is known for fostering a positive workplace culture
-                  and navigating complex HR challenges with a results-oriented
-                  approach. Strong communicator and leader committed to
-                  promoting diversity, inclusion, and professional growth within
-                  Value Creed
-                </a>
-              </h3>
-            </article>
-            <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
-              <div className="relative mb-4 rounded-2xl">
-                <img
-                  className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
-                  src={judge3}
-                  alt="Prassan Iyer"
-                />
-                <LinkedInLink
-                  profileUrl={"https://www.linkedin.com/in/iyerprasanna/"}
-                />
-              </div>
-              <div className="flex justify-between items-center w-full pb-4 mb-auto">
-                <div className="flex items-center">
-                  <div className="flex flex-1">
-                    <div>
-                      <p className="text-sm font-semibold">
-                        Prassan <span className="text-[#27A5EF]">Iyer</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-end">
-                  <div className="text-sm flex items-center gap-2 text-white">
-                    Director of Technology
-                    <FaUserTie />
-                  </div>
-                </div>
-              </div>
-              <h3 className="font-medium text-xl leading-8">
-                <a
-                  href="#2"
-                  className="block relative group-hover:text-[#27A5EF] text-left transition-colors duration-200"
-                >
-                  Prasanna has a rich knowledge of the energy/commodities domain
-                  as a technology leader. He has developed & materialized
-                  tactical competency roadmaps for techno- functional expertise
-                  & currently serving as a director at Value Creed.
-                </a>
-              </h3>
-            </article>
+          <div className="md:grid md:gap-6 md:grid-cols-2 lg:grid-cols-2 justify-center">
             <div className="md:flex md:justify-center md:col-span-3 md:gap-10 md:mb-36 md:ml-64 md:mr-64">
-              <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
-                <div className="relative mb-4 rounded-2xl">
+              <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer ">
+                <div className="relative mb-4 rounded-2xl overflow-hidden h-80">
                   <img
                     className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
-                    src="https://images.unsplash.com/photo-1621967314091-f7151d92bf49?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                    alt=""
+                    src={judge1}
+                    alt="Sakshi Saini"
                   />
-                  <LinkedInLink profileUrl={"Linkdin"} />
+                  <LinkedInLink
+                    profileUrl={
+                      "https://www.linkedin.com/in/sakshi-saini-741414181/"
+                    }
+                  />
                 </div>
                 <div className="flex justify-between items-center w-full pb-4 mb-auto">
                   <div className="flex items-center">
                     <div className="flex flex-1">
                       <div>
                         <p className="text-sm font-semibold">
-                          Lorem <span className="text-[#27A5EF]">Ipsum</span>
+                          Sakshi <span className="text-[#27A5EF]">Saini</span>
                         </p>
                       </div>
                     </div>
                   </div>
                   <div className="flex justify-end">
-                    <div className="text-sm flex items-center text-gray-500">
-                      Occupation
-                      {/* give some sort of icon here representing the occupation */}
+                    <div className="text-sm flex items-center text-white gap-2">
+                      HR Associate
+                      <BsPersonLinesFill className="" />
                     </div>
                   </div>
                 </div>
                 <h3 className="font-medium text-xl leading-8">
                   <a
                     href="#2"
-                    className="block relative group-hover:text-[#27A5EF] transition-colors duration-200"
+                    className=" max-w-lg block relative group-hover:text-[#27A5EF] text-left transition-colors duration-200"
                   >
-                    Lorem ipsum dolor sit amet, Sed ad minus quam voluptate?
-                    {/* this is some line about the speaker or the designation */}
+                    Sakshi has been awarded an MBA degree in Human Resources from
+                    UPES, Dehradun. Currently serving as a HR Associate at Value
+                    Creed and doing end to end recruitment globally.
                   </a>
                 </h3>
               </article>
@@ -177,6 +60,48 @@ function Judges() {
                 <div className="relative mb-4 rounded-2xl">
                   <img
                     className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+                    src={judge3}
+                    alt="Prassan Iyer"
+                  />
+                  <LinkedInLink
+                    profileUrl={"https://www.linkedin.com/in/iyerprasanna/"}
+                  />
+                </div>
+                <div className="flex justify-between items-center w-full pb-4 mb-auto">
+                  <div className="flex items-center">
+                    <div className="flex flex-1">
+                      <div>
+                        <p className="text-sm font-semibold">
+                          Prassan <span className="text-[#27A5EF]">Iyer</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex justify-end">
+                    <div className="text-sm flex items-center gap-2 text-white">
+                      Director of Technology
+                      <FaUserTie />
+                    </div>
+                  </div>
+                </div>
+                <h3 className="font-medium text-xl leading-8">
+                  <a
+                    href="#2"
+                    className=" max-w-lg block relative group-hover:text-[#27A5EF] text-left transition-colors duration-200"
+                  >
+                    Prasanna has a rich knowledge of the energy/commodities domain
+                    as a technology leader. He has developed & materialized
+                    tactical competency roadmaps for techno- functional expertise
+                    & currently serving as a director at Value Creed.
+                  </a>
+                </h3>
+              </article>
+            </div>
+            <div className="md:flex md:justify-center md:col-span-3 md:gap-10 md:mb-36 md:ml-64 md:mr-64">
+              <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
+                <div className="relative mb-4 rounded-2xl overflow-hidden h-80">
+                  <img
+                    className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
                     src="https://images.unsplash.com/photo-1621967314091-f7151d92bf49?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
                     alt=""
                   />
@@ -202,7 +127,43 @@ function Judges() {
                 <h3 className="font-medium text-xl leading-8">
                   <a
                     href="#2"
-                    className="block relative group-hover:text-[#27A5EF] transition-colors duration-200"
+                    className=" max-w-lg block relative group-hover:text-[#27A5EF] transition-colors duration-200"
+                  >
+                    Lorem ipsum dolor sit amet, Sed ad minus quam voluptate?
+                    {/* this is some line about the speaker or the designation */}
+                  </a>
+                </h3>
+              </article>
+              <article className="shadow-white text-white p-6 mb-6 border border-gray-100 transition duration-300 group transform hover:-translate-y-2 hover:shadow-2xl rounded-2xl cursor-pointer">
+                <div className="relative mb-4 rounded-2xl overflow-hidden h-80">
+                  <img
+                    className="max-h-80 rounded-2xl w-full object-cover transition-transform duration-300 transform group-hover:scale-105"
+                    src="https://images.unsplash.com/photo-1621967314091-f7151d92bf49?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfGhtZW52UWhVbXhNfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                    alt=""
+                  />
+                  <LinkedInLink profileUrl={"Linkdin"} />
+                </div>
+                <div className="flex justify-between items-center w-full pb-4 mb-auto">
+                  <div className="flex items-center">
+                    <div className="flex flex-1">
+                      <div>
+                        <p className="text-sm font-semibold">
+                          Lorem <span className="text-[#27A5EF]">Ipsum</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex justify-end">
+                    <div className="text-sm flex items-center text-gray-500">
+                      Occupation
+                      {/* give some sort of icon here representing the occupation */}
+                    </div>
+                  </div>
+                </div>
+                <h3 className="font-medium text-xl leading-8">
+                  <a
+                    href="#2"
+                    className=" max-w-lg block relative group-hover:text-[#27A5EF] transition-colors duration-200"
                   >
                     Lorem ipsum dolor sit amet, Sed ad minus quam voluptate?
                     {/* this is some line about the speaker or the designation */}
@@ -213,7 +174,6 @@ function Judges() {
           </div>
 
           <div className="md:grid md:gap-6 md:grid-cols-2 lg:grid-cols-3 mb">
-            {/* More articles */}
           </div>
         </div>
       </div>
